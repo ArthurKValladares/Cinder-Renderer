8a99f315f12245be06ea9f63bf9fd79b8d2666a5
ArthurValladares <arthurkaukal@gmail.com>
2022-09-03 18:07:21 -0700

    create vulkan physical device

--- cinder/src/backend/vulkan/mod.rs
+++ cinder/src/backend/vulkan/mod.rs
@@ -25,10 +25,18 @@ fn extensions() -> Vec<&'static CStr> {
 pub enum RendererContextInitError {
     #[error(transparent)]
     LoadingError(#[from] ash::LoadingError),
+    #[error("Failed to enumerate window extensions {0}")]
+    FailedToEnumerateWindowExtensions(ash::vk::Result),
     #[error("Failed to create instance {0}")]
     InstanceCreationFailed(ash::vk::Result),
     #[error("Failed to create debug utils {0}")]
     DebugUtilsCreationFailed(ash::vk::Result),
+    #[error("Failed to find a physical device {0}")]
+    NoPhysicalDevice(ash::vk::Result),
+    #[error("Failed to find a supported physical device")]
+    NoSupportedPhysicalDevice,
+    #[error("Failed to create surface {0}")]
+    FailedToCreateSurface(ash::vk::Result),
 }
 
 pub struct RendererContext {
@@ -36,6 +44,9 @@ pub struct RendererContext {
     instance: ash::Instance,
     debug_utils: ash::extensions::ext::DebugUtils,
     debug_utils_messenger: vk::DebugUtilsMessengerEXT,
+    p_device: vk::PhysicalDevice,
+    queue_family_index: usize,
+    p_device_properties: vk::PhysicalDeviceProperties,
 }
 
 impl AsRendererContext for RendererContext {
@@ -52,12 +63,17 @@ impl AsRendererContext for RendererContext {
             .collect::<Vec<*const c_char>>();
 
         // TODO: Configurable
-        let window_extensions = ash_window::enumerate_required_extensions(window);
         let extensions = extensions();
-        let extensions = extensions
-            .iter()
-            .map(|raw_name| raw_name.as_ptr())
-            .collect::<Vec<*const c_char>>();
+        let extensions = {
+            let window_extensions = ash_window::enumerate_required_extensions(window)
+                .map_err(RendererContextInitError::FailedToEnumerateWindowExtensions)?;
+            let mut extensions = extensions
+                .iter()
+                .map(|raw_name| raw_name.as_ptr())
+                .collect::<Vec<*const c_char>>();
+            extensions.extend(window_extensions.iter());
+            extensions
+        };
 
         let app_info = vk::ApplicationInfo::builder().api_version(vk::make_api_version(0, 1, 3, 0)); // TODO: Configure version
         let instance_ci = vk::InstanceCreateInfo::builder()
@@ -89,11 +105,60 @@ impl AsRendererContext for RendererContext {
                 .map_err(RendererContextInitError::DebugUtilsCreationFailed)?
         };
 
+        let surface_loader = ash::extensions::khr::Surface::new(&entry, &instance);
+        let surface = unsafe { ash_window::create_surface(&entry, &instance, window, None) }
+            .map_err(RendererContextInitError::FailedToCreateSurface)?;
+
+        let p_devices = unsafe { instance.enumerate_physical_devices() }
+            .map_err(RendererContextInitError::NoPhysicalDevice)?;
+        let supported_device_data = p_devices
+            .into_iter()
+            .flat_map(|p_device| {
+                unsafe { instance.get_physical_device_queue_family_properties(p_device) }
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(index, info)| {
+                        let supports_graphic_and_surface =
+                            info.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+                                && unsafe {
+                                    surface_loader.get_physical_device_surface_support(
+                                        p_device,
+                                        index as u32,
+                                        surface,
+                                    )
+                                }
+                                .unwrap_or(false);
+                        if supports_graphic_and_surface {
+                            let properties =
+                                unsafe { instance.get_physical_device_properties(p_device) };
+                            Some((p_device, index, properties))
+                        } else {
+                            None
+                        }
+                    })
+                    .next()
+            })
+            .collect::<Vec<_>>();
+        let (p_device, queue_family_index, p_device_properties) = supported_device_data
+            .into_iter()
+            .rev()
+            .max_by_key(
+                |(device, queue_family_index, properties)| match properties.device_type {
+                    vk::PhysicalDeviceType::INTEGRATED_GPU => 200,
+                    vk::PhysicalDeviceType::DISCRETE_GPU => 1000,
+                    vk::PhysicalDeviceType::VIRTUAL_GPU => 1,
+                    _ => 0,
+                },
+            )
+            .ok_or(RendererContextInitError::NoSupportedPhysicalDevice)?;
         Ok(RendererContext {
             entry,
             instance,
             debug_utils,
             debug_utils_messenger,
+            p_device,
+            queue_family_index,
+            p_device_properties,
         })
     }
 }
