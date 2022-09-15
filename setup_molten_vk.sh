31bd2dc7a563bdbde3d4e8878cb1953681169426
Arthur Valladares <arthur@brainiumstudios.com>
2022-09-15 13:47:24 -0700

    tweaks to macos setup

--- setup_molten_vk.sh
+++ setup_molten_vk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export VULKAN_SDK=$HOME/VulkanSDK/1.3.224.1/macOS
-export PATH=$VULKAN_SDK/bin:$PATH
-export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib:$DYLD_LIBRARY_PATH
-export VK_ICD_FILENAMES=$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json
-export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
+export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib
+export VK_ICD_FILENAMES=$VULKAN_SDK/share/vulkan/icd.d/MoltenVK_icd.json
+export VK_LAYER_PATH=$VULKAN_SDK/share/vulkan/explicit_layer.d
