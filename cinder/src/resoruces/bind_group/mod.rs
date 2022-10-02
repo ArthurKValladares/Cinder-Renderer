17572db2afa6be8412d2da5c1c1fef2166f076b9
ArthurValladares <arthurkaukal@gmail.com>
2022-10-01 22:04:34 -0700

    BindGroupLayoutCache

--- cinder/src/resoruces/bind_group/mod.rs
+++ cinder/src/resoruces/bind_group/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use ash::vk;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct BindGroupDesc {
@@ -171,6 +172,70 @@ impl BindGroupAllocator {
             }
             for pool in self.free_pools.drain(..) {
                 device.destroy_descriptor_pool(pool, None);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BindGroupLayoutBinding(vk::DescriptorSetLayoutBinding);
+
+impl Eq for BindGroupLayoutBinding {}
+impl PartialEq for BindGroupLayoutBinding {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.binding == other.0.binding
+            && self.0.descriptor_type == other.0.descriptor_type
+            && self.0.descriptor_count == other.0.descriptor_count
+            && self.0.stage_flags == other.0.stage_flags
+            && self.0.p_immutable_samplers == other.0.p_immutable_samplers
+    }
+}
+
+impl std::hash::Hash for BindGroupLayoutBinding {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.binding.hash(state);
+        self.0.descriptor_type.hash(state);
+        self.0.descriptor_count.hash(state);
+        self.0.stage_flags.hash(state);
+        self.0.p_immutable_samplers.hash(state);
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct BindGroupLayoutInfo {
+    bindings: Vec<BindGroupLayoutBinding>,
+}
+
+#[derive(Debug, Default)]
+pub struct BindGroupLayoutCache {
+    cache: HashMap<BindGroupLayoutInfo, vk::DescriptorSetLayout>,
+}
+
+impl BindGroupLayoutCache {
+    pub fn create_bind_group_layout(
+        &mut self,
+        device: &ash::Device,
+        info: vk::DescriptorSetLayoutCreateInfo,
+    ) -> Result<vk::DescriptorSetLayout, vk::Result> {
+        let mut bindings = Vec::with_capacity(info.binding_count as usize);
+        for i in (0..info.binding_count) {
+            bindings.push(BindGroupLayoutBinding(unsafe { *info.p_bindings }));
+        }
+        bindings.sort_by(|left, right| left.0.binding.partial_cmp(&right.0.binding).unwrap());
+        let layout_info = BindGroupLayoutInfo { bindings };
+        if let Some(layout) = self.cache.get(&layout_info) {
+            Ok(*layout)
+        } else {
+            let layout = unsafe { device.create_descriptor_set_layout(&info, None) }?;
+            self.cache.insert(layout_info, layout);
+            Ok(layout)
+        }
+    }
+
+    pub fn cleanup(&mut self, device: &ash::Device) {
+        unsafe {
+            for (_, layout) in self.cache.drain() {
+                device.destroy_descriptor_set_layout(layout, None);
             }
         }
     }
