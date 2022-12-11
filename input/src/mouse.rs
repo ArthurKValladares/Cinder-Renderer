7cb1dde84fcaf3b3769c628258af816e60cc80ec
ArthurValladares <arthurkaukal@gmail.com>
2022-12-10 20:38:15 -0800

    more MouseState stuff

--- input/src/mouse.rs
+++ input/src/mouse.rs
@@ -1,25 +1,78 @@
-use math::{size::Size2D, vec::Vec2};
+use math::vec::Vec2;
+pub use winit::event::MouseButton;
+use winit::{
+    dpi::PhysicalPosition,
+    event::{ElementState, Event, WindowEvent},
+};
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct MouseState {
-    pub delta: Option<Vec2>,
+    pub position: PhysicalPosition<f64>,
+    pub delta: Vec2,
+    pub buttons_held: u32,
+    pub buttons_pressed: u32,
+    pub buttons_released: u32,
+}
+
+fn button_id(mouse_button: &MouseButton) -> u32 {
+    match mouse_button {
+        MouseButton::Left => 0,
+        MouseButton::Middle => 1,
+        MouseButton::Right => 2,
+        _ => 0,
+    }
 }
 
 impl MouseState {
-    pub fn update(&mut self, window_size: Size2D<u32>, cursor_delta: Option<(f32, f32)>) {
-        let checked_div = |numerator, denominator| {
-            if denominator == 0 {
-                0.0 as f32
-            } else {
-                numerator as f32 / denominator as f32
-            }
-        };
+    pub fn update(&mut self, events: &[Event<'_, ()>]) {
+        self.delta = Vec2::zero();
 
-        self.delta = cursor_delta.map(|(x, y)| {
-            Vec2::new(
-                checked_div(x, window_size.width()),
-                checked_div(y, window_size.height()),
-            )
-        });
+        self.buttons_released = 0;
+        self.buttons_pressed = 0;
+
+        for event in events {
+            match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CursorMoved { position, .. } => {
+                        self.position = *position;
+                    }
+                    WindowEvent::MouseInput { state, button, .. } => {
+                        let button_id = button_id(button);
+
+                        match state {
+                            ElementState::Pressed => {
+                                self.buttons_held |= 1 << button_id;
+                                self.buttons_pressed |= 1 << button_id;
+                            }
+                            ElementState::Released => {
+                                self.buttons_held &= !(1 << button_id);
+                                self.buttons_released |= 1 << button_id;
+                            }
+                        }
+                    }
+                    _ => (),
+                },
+                Event::DeviceEvent {
+                    device_id: _,
+                    event: winit::event::DeviceEvent::MouseMotion { delta },
+                } => {
+                    self.delta.x += delta.0 as f32;
+                    self.delta.y += delta.1 as f32;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    pub fn was_just_pressed(&self, mouse_button: MouseButton) -> bool {
+        (self.buttons_pressed & 1 << button_id(&mouse_button)) != 0
+    }
+
+    pub fn was_just_released(&self, mouse_button: MouseButton) -> bool {
+        (self.buttons_released & 1 << button_id(&mouse_button)) != 0
+    }
+
+    pub fn is_held(&self, mouse_button: &MouseButton) -> bool {
+        (self.buttons_held & 1 << button_id(&mouse_button)) != 0
     }
 }
