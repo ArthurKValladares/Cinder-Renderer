ca444881f847712d63e6b051e68646c214dd8abf
ArthurValladares <arthurkaukal@gmail.com>
2023-01-15 23:42:12 -0800

    a bit more work on mesh loading

--- crates/lib/scene_2/src/lib.rs
+++ crates/lib/scene_2/src/lib.rs
@@ -1,9 +1,9 @@
-use std::path::Path;
-
 use anyhow::Result;
+use std::path::Path;
+pub use tobj::Mesh as ObjMesh;
 
 pub trait Vertex {
-    fn from_obj_mesh_index(mesh: &tobj::Mesh, index: usize) -> Self;
+    fn from_obj_mesh_index(mesh: &ObjMesh, i: usize) -> Self;
 
     fn pos_3d(&self) -> [f32; 3];
 }
