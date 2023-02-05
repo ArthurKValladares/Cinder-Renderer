e4ad6bb6f64799558e6d71b188d70e119b11af90
ArthurValladares <arthurkaukal@gmail.com>
2023-02-04 21:41:30 -0800

    delete blur example

--- crates/bin/blur/gen/blur_texture_shader_structs.rs
+++
@@ -1,7 +0,0 @@
-#[repr(C)]
-#[derive(Debug, Default, Copy, Clone)]
-#[derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)]
-pub struct BlurTextureVertex {
-    pub i_pos: [f32; 2],
-    pub i_uv: [f32; 2],
-}
--- crates/bin/blur/shaders/blur_mesh.frag
+++
@@ -1,9 +0,0 @@
-#version 450
-
-layout (location = 0) in vec4 i_color;
-
-layout (location = 0) out vec4 uFragColor;
-
-void main() {
-    uFragColor = i_color;
-}
