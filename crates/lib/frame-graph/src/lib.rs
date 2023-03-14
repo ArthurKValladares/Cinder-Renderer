f44a2c92bea6fc8edb9510af47905d6457e68047
ArthurValladares <arthurkaukal@gmail.com>
2023-03-13 22:14:53 -0700

    FrameGraph stuff, idk what im doing

--- crates/lib/frame-graph/src/lib.rs
+++ crates/lib/frame-graph/src/lib.rs
@@ -1,161 +1,53 @@
-use cinder::resources::{buffer::BufferUsage, image::Format};
-use math::size::Size3D;
-use thiserror::Error;
+use cinder::{
+    resources::{image::Image, pipeline::graphics::GraphicsPipeline},
+    ResourceHandle,
+};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
 
-#[derive(Debug, Copy, Clone)]
-pub struct PassResourceHandle(usize);
-
-impl PassResourceHandle {
-    // TOOD: temp for testing
-    pub fn new(inner: usize) -> Self {
-        Self(inner)
-    }
+#[derive(Debug, Hash)]
+pub struct PassInfo {
+    name: String,
+    pipeline_handle: ResourceHandle<GraphicsPipeline>,
+    input_image: ResourceHandle<Image>,
+    depth_image: Option<ResourceHandle<Image>>,
+    viewport_size: [u32; 2],
 }
 
 #[derive(Debug)]
-pub enum SizeClass {
-    Absolute(Size3D<u32>),
-    SwapchainRelative(Size3D<f32>),
-    InputRelative(Size3D<f32>),
+pub struct Pass;
+
+#[derive(Debug, Default)]
+pub struct FrameGraph {
+    pass_infos: Vec<(ResourceHandle<Pass>, PassInfo)>,
 }
 
-#[derive(Debug)]
-pub struct ImageInfo {
-    pub handle: PassResourceHandle,
-    pub size_class: SizeClass,
-    pub format: Format,
-    pub persistent: bool,
-}
-
-#[derive(Debug)]
-pub struct BufferInfo {
-    pub handle: PassResourceHandle,
-    pub size: usize,
-    pub usage: BufferUsage,
-    pub persistent: bool,
-}
-
-#[derive(Debug)]
-pub enum PassResource {
-    Image(ImageInfo),
-    Buffer(BufferInfo),
-}
-
-#[derive(Debug, Error)]
-pub enum PassError {
-    #[error("size of color inputs must match color outputs")]
-    ColorAttachmentsMismatch,
-}
-
-#[derive(Debug)]
-pub struct Pass<'a> {
-    name: &'a str,
-    color_inputs: Vec<PassResourceHandle>,
-    color_outputs: Vec<PassResource>,
-}
-
-impl<'a> Pass<'a> {
-    pub fn new(name: &'a str) -> Self {
-        Self {
-            name,
-            color_inputs: Default::default(),
-            color_outputs: Default::default(),
-        }
+impl FrameGraph {
+    pub fn add_pass(&mut self, pass_info: PassInfo) -> ResourceHandle<Pass> {
+        let handle = {
+            let mut hasher = DefaultHasher::new();
+            pass_info.hash(&mut hasher);
+            ResourceHandle::from_index(hasher.finish() as usize)
+        };
+        self.pass_infos.push((handle, pass_info));
+        handle
     }
 
-    pub fn extend_color_inputs(&mut self, inputs: impl IntoIterator<Item = PassResourceHandle>) {
-        self.color_inputs.extend(inputs)
-    }
-
-    pub fn add_color_input(&mut self, res: PassResourceHandle) {
-        self.color_inputs.push(res)
-    }
-
-    pub fn extend_color_outputs(&mut self, outputs: impl IntoIterator<Item = PassResource>) {
-        self.color_outputs.extend(outputs)
-    }
-
-    pub fn add_color_output(&mut self, handle: PassResource) {
-        self.color_outputs.push(handle)
-    }
-}
-
-#[derive(Debug)]
-pub struct FrameGraph<'a> {
-    name: &'a str,
-    passes: Vec<Pass<'a>>,
-}
-
-impl<'a> FrameGraph<'a> {
-    pub fn new(name: &'a str) -> Self {
-        Self {
-            name,
-            passes: Default::default(),
-        }
-    }
-
-    pub fn with_passes(name: &'a str, passes: impl IntoIterator<Item = Pass<'a>>) -> Self {
-        Self {
-            name,
-            passes: passes.into_iter().collect(),
-        }
-    }
-
-    pub fn add_pass(&mut self, pass: Pass<'a>) {
-        self.passes.push(pass);
-    }
-
-    fn validate_passes(&self) -> Result<(), PassError> {
-        for pass in &self.passes {
-            if pass.color_inputs.len() != pass.color_outputs.len() {
-                return Err(PassError::ColorAttachmentsMismatch);
-            }
-        }
-        Ok(())
-    }
-
-    pub fn bake(self) -> Result<(), PassError> {
-        self.validate_passes()?;
-
-        Ok(())
+    pub fn build_graph(&self) -> ResourceHandle<FrameGraph> {
+        // TODO
+        ResourceHandle::from_index(0)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{FrameGraph, ImageInfo, Pass, PassResource, PassResourceHandle, SizeClass};
-    use cinder::resources::image::Format;
-    use math::size::Size3D;
+    use super::FrameGraph;
 
     #[test]
     fn api() {
-        let mut frame_graph = FrameGraph::new("frame_graph");
-
-        let albedo_handle = PassResourceHandle::new(0);
-        let depth_handle = PassResourceHandle::new(1);
-
-        let mut g_buffer_pass = Pass::new("g_buffer");
-        g_buffer_pass.extend_color_outputs(vec![
-            PassResource::Image(ImageInfo {
-                handle: albedo_handle,
-                size_class: SizeClass::SwapchainRelative(Size3D::new(1.0, 1.0, 1.0)),
-                format: Format::R8G8B8A8_Unorm,
-                persistent: false,
-            }),
-            PassResource::Image(ImageInfo {
-                handle: depth_handle,
-                size_class: SizeClass::SwapchainRelative(Size3D::new(1.0, 1.0, 1.0)),
-                format: Format::D32_SFloat,
-                persistent: false,
-            }),
-        ]);
-        frame_graph.add_pass(g_buffer_pass);
-
-        let mut lighting_pass = Pass::new("lighting");
-        lighting_pass.extend_color_inputs(vec![albedo_handle, depth_handle]);
-        frame_graph.add_pass(lighting_pass);
-
-        let bake_result = frame_graph.bake();
-        assert!(bake_result.is_ok(), "Error: {bake_result:?}");
+        let mut frame_graph = FrameGraph::default();
+        // TODO: Actually hook up tests, will take a bit more work
     }
 }
