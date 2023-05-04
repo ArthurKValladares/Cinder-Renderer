7df76fd9508f06472d41b82f171580fb404a4572
ArthurKV <arthur@brainiumstudios.com>
2023-05-04 15:20:37 -0700

    a bit more work on cylinder

--- crates/lib/geometry/src/lib.rs
+++ crates/lib/geometry/src/lib.rs
@@ -8,47 +8,42 @@ pub struct SurfaceMesh {
 
 impl SurfaceMesh {
     pub fn cylinder<const N: usize>(height: f32, radius: f32) -> Self {
-        let mut vertices: Vec<Vertex> = Default::default();
-        let mut indices: Vec<u32> = Default::default();
+        let top_offset = N + 1;
+        let mut vertices: Vec<Vertex> = vec![Default::default(); (N + 1) * 2];
+        let mut indices: Vec<u32> = vec![0; (N + 1) * 6 + 6];
 
-        // Generate top and bottom vertices
-        let mut bottom_vertices = [Vertex::default(); N];
-        let mut top_vertices = [Vertex::default(); N];
-        for i in 0..N - 1 {
+        for i in 0..N {
             let ratio = i as f32 / N as f32;
             let angle = ratio * std::f32::consts::PI * 2.0;
             let x = angle.cos() * radius;
             let z = angle.sin() * radius;
-            bottom_vertices[i] = Vertex { pos: [x, 0.0, z] };
-            top_vertices[i] = Vertex {
+            vertices[i] = Vertex { pos: [x, 0.0, z] };
+            vertices[i + top_offset] = Vertex {
                 pos: [x, height, z],
             };
         }
-        bottom_vertices[N - 1] = Vertex {
+        vertices[N] = Vertex {
             pos: [0.0, 0.0, 0.0],
         };
-        top_vertices[N - 1] = Vertex {
+        vertices[N + top_offset] = Vertex {
             pos: [0.0, height, 0.0],
         };
 
         // Generate indices
-        let last_idx = (N - 1) as u32;
-        let mut bottom_indices = vec![];
-        let mut top_indices = vec![];
-        for i in 0..N - 1 {
-            let i = i as u32;
-            bottom_indices.extend([i, i + 1, last_idx]);
-            top_indices.extend([i + 1, i, last_idx]);
+        for i in 0..N {
+            let to = top_offset as u32;
+
+            //Bottom triangle
+            indices[i * 3] = i as u32;
+            indices[i * 3 + 1] = i as u32 + 1;
+            indices[i * 3 + 2] = N as u32;
+
+            // Top Triangle
+            indices[N * 3 + i * 3] = to + i as u32 + 1;
+            indices[N * 3 + i * 3 + 1] = to + i as u32;
+            indices[N * 3 + i * 3 + 2] = to + N as u32;
         }
-        bottom_indices.extend([last_idx - 1, 0, last_idx]);
-        top_indices.extend([0, last_idx - 1, last_idx]);
-
-        // Move all to final vector (wont exist as step at the end)
-        vertices.extend(bottom_vertices);
-        vertices.extend(top_vertices);
-
-        indices.extend(bottom_indices);
-        indices.extend(top_indices);
+        // TODO: close circles, add walls
 
         Self { vertices, indices }
     }
