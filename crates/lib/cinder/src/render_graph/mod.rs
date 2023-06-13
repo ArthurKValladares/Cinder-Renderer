12de78d082748cf208adab2b8c19ad00db737c1b
ArthurValladares <arthurkaukal@gmail.com>
2023-06-12 21:52:43 -0700

    some more work that i'm not using yet

--- crates/lib/cinder/src/render_graph/mod.rs
+++ crates/lib/cinder/src/render_graph/mod.rs
@@ -1,15 +1,37 @@
-use crate::device::Device;
+use crate::{
+    device::Device,
+    resources::{
+        buffer::Buffer,
+        image::{Image, ImageUsage, Layout},
+    },
+};
 use anyhow::Result;
-use resource_manager::ResourceId;
+
+pub struct ImageResource<T> {
+    f: Box<dyn Fn(&Device, &mut T) -> Image>,
+    aspect_mask: ImageUsage,
+    old_layout: Layout,
+    new_layout: Layout,
+}
+
+pub struct BufferResource<T> {
+    f: Box<dyn Fn(&Device, &mut T) -> Buffer>,
+    // TODO: Buffer memory barrier stuff
+}
+
+pub enum Resource<T> {
+    Buffer(BufferResource<T>),
+    Image(ImageResource<T>),
+}
 
 pub struct Task<T, F: FnMut(&Device) -> Result<()>> {
-    resources: Vec<ResourceId<T>>,
+    resources: Vec<Resource<T>>,
     task: F,
 }
 
 pub type NodeFn<'a> = dyn FnMut(&Device) -> Result<()> + 'a;
 pub struct Node<'a, T> {
-    resources: Vec<ResourceId<T>>,
+    resources: Vec<Resource<T>>,
     task: Box<NodeFn<'a>>,
 }
 
